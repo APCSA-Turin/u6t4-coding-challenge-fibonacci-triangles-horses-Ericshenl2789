bd5d169af2d02cdcccfcf640ee402797f037b05e
Eric Lin <ericshenl2789@nycstudents.net>
2025-01-07 00:42:59 +0000

    commit

--- src/main/java/com/example/project/Point.java
+++ src/main/java/com/example/project/Point.java
@@ -1,22 +1,40 @@
 package com.example.project;
 
 public class Point {
-    private int x;
-    private int y;
-    
-    public Point(int x, int y) {
-      this.x = x;
-      this.y = y;
-    }
-    
-    // Returns distance between this Point object and another Point object
-    public double distanceTo(Point other) {
-      /* IMPLEMENT ME */
-    }
-  
-    // Returns a string in the format: (x, y)
-    public String pointInfo() {
-      /* IMPLEMENT ME */
-    }
+  private int x;
+  private int y;
+ 
+  public Point(int x, int y) {
+    this.x = x;
+    this.y = y;
   }
-  
+ 
+  // Returns distance between this Point object and another Point object
+  public double distanceTo(Point other) {
+      return Math.sqrt(Math.pow((other.x - x), 2) + Math.pow(other.y - y, 2));
+  }
+
+  public int getX() {
+      return x;
+  }
+
+
+  public int getY() {
+      return y;
+  }
+
+
+  public void setX(int x) {
+      this.x = x;
+  }
+ 
+  public void setY(int y) {
+      this.y = y;
+  }
+
+
+  // Returns a string in the format: (x, y)
+  public String pointInfo() {
+      return "(" + x + ", " + y + ")";
+  }
+}
